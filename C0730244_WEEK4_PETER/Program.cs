6fd26e7ec6211ffdd8ef194102ad09f6f25f9163
visualstudio <visualstudio@230-PC40>
2019-01-31 14:28:14 -0500

    coding

---
+++ C0730244_WEEK4_PETER/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C0730244_WEEK4_PETER
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
